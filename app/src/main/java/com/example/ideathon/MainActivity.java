5702db95cb6b9470db00b675a764c4a961c4d69b
Shivam Nagpal <shivamnagpal432@gmail.com>
2018-08-26 02:04:57 +0530

    Resource Management

--- app/src/main/java/com/example/ideathon/MainActivity.java
+++ app/src/main/java/com/example/ideathon/MainActivity.java
@@ -78,7 +78,6 @@ public class MainActivity extends AppCompatActivity implements AdsAdapter.ItemCl
                 }
                 Log.v("Places", "Size:" + placeArrayList.size());
                 responses.release();
-                responses.release();
 
                 for (Place p : placeArrayList) {
                     stringHashSet.add(p.getName().toString().trim().toLowerCase());
@@ -119,7 +118,7 @@ public class MainActivity extends AppCompatActivity implements AdsAdapter.ItemCl
         if (firebaseDatabase == null) {
             firebaseDatabase = FirebaseDatabase.getInstance();
             firebaseDatabase.setPersistenceEnabled(true);
-            firebaseDatabase.setPersistenceCacheSizeBytes(5* 1024 * 1024);
+            firebaseDatabase.setPersistenceCacheSizeBytes(5 * 1024 * 1024);
         }
     }
 
